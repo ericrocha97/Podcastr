ae7fa25e0fae1aa90686af14c0f35d92425b8108
Eric Rocha <ericrochaeric@gmail.com>
2021-04-22 02:22:25 -0300

    aula 03

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -7,7 +8,9 @@ export function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', { locale: ptBR })
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
+      <Link href="/">
+        <img src="/logo.svg" alt="Podcastr" />
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
